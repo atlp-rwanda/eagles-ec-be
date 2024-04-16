e9a345d2717ce508093fa224a401320e7b4b151e
soleil00 <srukundo01@gmail.com>
2024-04-16 10:31:51 +0200

    chore : Set up Tests locally & implement Continuous Integration

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -5,13 +5,31 @@ module.exports = {
   development: {
     url: process.env.DB_CONNECTION,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     url: process.env.DB_CONNECTION,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     url: process.env.DB_CONNECTION,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
