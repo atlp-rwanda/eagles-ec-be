416b89c11e4d4b53090ae69c7d4dbe19c1ccccb3
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2024-04-16 21:19:29 +0200

    Configure ESLint and Git Pre commit hook to run tests and ESlint forma

--- __test__/user.test.ts
+++ __test__/user.test.ts
@@ -1,25 +1,27 @@
-import request from "supertest";
-import { beforeAll, afterAll, jest, test } from "@jest/globals";
-import app from "../src/utils/server";
-import { testDbConnection, testSequelize } from "../src/config/testDbConfig";
-import User from "../src/sequelize/models/user";
-import * as userServices from "../src/services/user.service";
+import request from 'supertest'
+import { beforeAll, afterAll, jest, test } from '@jest/globals'
+import app from '../src/utils/server'
+import { testDbConnection, testSequelize } from '../src/config/testDbConfig'
+import User from '../src/sequelize/models/user'
+import * as userServices from '../src/services/user.service'
 
-describe("Testing user Routes", () => {
+describe('Testing user Routes', () => {
   beforeAll(async () => {
     try {
-      await testDbConnection();
+      await testDbConnection()
     } catch (error) {
-      testSequelize.close();
+      testSequelize.close()
     }
-  }, 20000);
+  }, 20000)
 
   test("should return all users in db --> given '/api/v1/users'", async () => {
-    const spy = jest.spyOn(User, "findAll");
-    const spy2 = jest.spyOn(userServices, "getAllUsers");
-    const response = await request(app).get("/api/v1/users");
-    expect(spy).toHaveBeenCalled();
-    expect(spy2).toHaveBeenCalled();
-    expect(response.statusCode).toBe(200);
-  }, 20000);
-});
+    const spy = jest.spyOn(User, 'findAll')
+    const spy2 = jest.spyOn(userServices, 'getAllUsers')
+    const response = await request(app).get('/api/v1/users')
+
+    expect(spy).toHaveBeenCalled()
+
+    expect(spy2).toHaveBeenCalled()
+    expect(response.statusCode).toBe(200)
+  }, 20000)
+})
