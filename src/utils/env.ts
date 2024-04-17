6ff47c9b4ebb1d478c0d7db73adc4f5bb5731062
soleil00 <srukundo01@gmail.com>
2024-04-17 11:36:29 +0200

    chore : Set up Tests locally & implement Continuous Integration

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -5,4 +5,5 @@ export const env = {
   port: process.env.PORT || 3000,
   db_url: process.env.DB_CONNECTION as string,
   jwt_secret:process.env.JWT_SECRET,
+  test_db_url: process.env.TEST_DB as string,
 };
