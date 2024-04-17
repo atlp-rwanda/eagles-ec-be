936c396866d729d5c6fbdaa7882b545d62c7b327
niyobertin <niyonkurubbertin@gmail.com>
2024-04-17 18:44:34 +0200

    feat(login)implement swagger documentation & test
    -swagger documentartion on user login with eamil and password
    -test user login endpoint on successfull event and fail events
    
    [Finishes #187419169]

--- __test__/user.test.ts
+++ __test__/user.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { beforeAll, afterAll, jest, test } from "@jest/globals";
 import app from "../src/utils/server";
 import User from "../src/sequelize/models/user";
+import TestUser from "../src/sequelize/models/testUser";
 import * as userServices from "../src/services/user.service";
 import sequelize, { connect } from "../src/config/dbConnection";
 
@@ -23,12 +24,33 @@ describe("Testing user Routes", () => {
   }, 20000);
 
   test("Should return status 200 to indicate that user logged in ",async() =>{
-    const loggedInUser = {
-      email:"alice@example.com",
-      password:`test123`,
-    };
-    const  response = await request(app).post("api/v1/users/login")
+    const loggedInUser ={
+      email:"ivan@gmail.com",
+      password:`1234567`,
+    }
+    const spyonOne = jest.spyOn(User,"findOne").mockResolvedValueOnce({
+      //@ts-ignore
+      email:"ivan@gmail.com",
+      password:"$2a$10$zonhxs7J7h3ls7nv781Xoea1X4dJoui3t0/7gRPR3Ek4Rgzg6RAQe",
+    })
+    const  response = await request(app).post("/api/v1/users/login")
     .send(loggedInUser)
     expect(response.status).toBe(200);
+    spyonOne.mockRestore();
+  })
+  test("Should return status 401 to indicate Unauthorized user",async() =>{
+    const loggedInUser ={
+      email:"ivan@gmail.com",
+      password:`1234560`,
+    }
+    const spyonOne = jest.spyOn(User,"findOne").mockResolvedValueOnce({
+      //@ts-ignore
+      email:"ivan@gmail.com",
+      password:"$2a$10$zonhxs7J7h3ls7nv781Xoea1X4dJoui3t0/7gRPR3Ek4Rgzg6RAQe",
+    })
+    const  response = await request(app).post("/api/v1/users/login")
+    .send(loggedInUser)
+    expect(response.status).toBe(401);
+    spyonOne.mockRestore();
   })
 });
